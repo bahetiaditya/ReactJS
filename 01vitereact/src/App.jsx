b72dd18e83089bc501363931da1be960f88c21f8
imadityabaheti <bahetiaditya347@gmail.com>
2024-03-25 12:39:05 +0530

    01vitereact added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import One from "./one";
+
+function App() {
+  return (
+    <div>
+      <h1>Learning ReactJS | Using Vite.</h1>
+      <One></One>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/one.jsx
@@ -0,0 +1,5 @@
+function One() {
+  return <h3>js code from one.</h3>;
+}
+
+export default One;
