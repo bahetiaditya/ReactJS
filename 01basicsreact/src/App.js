38c7245fe0ad3c1859302d4b5194aa5c1245a42b
imadityabaheti <bahetiaditya347@gmail.com>
2024-03-25 12:37:23 +0530

    01basicsreact added

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,11 @@
+import One from "./one";
+function App() {
+  return (
+    <div>
+      <h1>ReactJS Learning | Basic Introduction.</h1>
+      <One></One>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicsreact/src/one.js
@@ -0,0 +1,5 @@
+function One() {
+  return <h3>js code with one.js</h3>;
+}
+
+export default One;
