1528e5987a9f129aedaf92a20ade642da71e590c
imadityabaheti <bahetiaditya347@gmail.com>
2024-03-26 12:23:09 +0530

    added new files and modified some existing files.

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import One from "./one";
 
 function App() {
+  const username = 'Aditya Baheti'
   return (
     <div>
-      <h1>Learning ReactJS | Using Vite.</h1>
+      <h1>Learning ReactJS | Using Vite, with {username}</h1>
       <One></One>
     </div>
   );
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function Myapp() {
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  );
+}
+
+const ReactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com/",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+
+const anotherUser = 'reactjs learning'
+
+const reactElement = React.createElement(
+  "a",
+  { href: 'https://google.com', target: "_blank" },
+
+  'click me to visit google',
+  anotherUser,
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  //  <App />
+  //   <Myapp />
+  // anotherElement,
+  reactElement
+);
